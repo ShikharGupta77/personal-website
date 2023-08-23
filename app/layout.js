c9b6e632cb7354f6d4333499ac1b0737644da67f
Shikhar Gupta <shikharscout@gmail.com>
2023-08-23 04:05:39 -0700

    Fixed favicon and metadata!

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,19 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Shikhar Gupta Personal Website",
+  title: "Shikhar Gupta Portfolio",
   description: "Shikhar Gupta's Personal Website",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <link rel="icon" href="/favicon.png" sizes="any" />
-      </Head>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
